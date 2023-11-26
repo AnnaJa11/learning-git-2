4c8b42a44eaac0a74f8caf37780394d2eeb2950c
Anna J <ajasion5@gmail.com>
2023-11-26 19:12:01 +0000

    change last print after total_items

--- main.py
+++ main.py
@@ -10,4 +10,4 @@ for k, v in new_list.items():
     print(f"Idę do {k}, kupuję tu następujące rzeczy: {v}.")
     
 total_items = len(lista_zakupow['piekarnia']) + len(lista_zakupow['warzywniak'])
-print(f'W sumie w koszyku mam {total_items} produktow.')
+print(f'W sumie w koszyku mam {total_items} produktow. I ide do kasy...')
